c4ad64274986dc4cbac8b47adaed79e3a2c37289
Roman <82847419+Roman-oze@users.noreply.github.com>
2024-10-18 22:11:03 +0600

    website setup

--- resources/views/include/_sidenav.blade.php
+++ resources/views/include/_sidenav.blade.php
@@ -1,159 +1,173 @@
-
 <div id="layoutSidenav_nav">
-        <nav class="sb-sidenav accordion sb-sidenav-dark " id="sidenavAccordion">
-            <div class="sb-sidenav-menu">
-                <div class="nav">
-
-                    <a class="nav-link" href="{{route('dashboard')}}">
-                        <div class="sb-nav-link-icon text-white "><i class="fas fa-tachometer-alt fa-lg"></i></div>
-                        Dashboard
-                    </a>
-                    <a class="nav-link collapsed  mt-3" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
-                        <div class="sb-nav-link-icon"><i class="fa-solid fa-users text-white fa-lg"></i></div>
-                        Users
-                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                    </a>
-                    <div class="collapse p-1 " id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
-                        <nav class="sb-sidenav-menu-nested nav drop-link " id="sidenavAccordionPages">
-                            <a  href="{{route('users.index')}}" class="linked "> Admins</a>
-                        </nav>
-                    </div>
-                    <div class="collapse p-1 " id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
-                        <nav class="sb-sidenav-menu-nested nav drop-link" id="sidenavAccordionPages">
-                            <a  href="" class="linked"> Upcoming</a>
-                        </nav>
-                    </div>
-
-
-
-                    <a class="nav-link collapsed mt-1" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                        <div class="sb-nav-link-icon"><i class="fa-solid fa-book-open  fa-lg text-white"></i></i></div>
-                         Students
-                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                    </a>
-                    <div class="collapse p-1" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                        <nav class="sb-sidenav-menu-nested nav drop-link">
-                            <a  href="{{route('student.index')}}" class="linked ">Students</a>
-                        </nav>
-                    </div>
-                    <div class="collapse p-1" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                        <nav class="sb-sidenav-menu-nested nav drop-link">
-                            <a  href="{{route('alumni.index')}}" class="linked">Alumni</a>
-                        </nav>
-                    </div>
-                    <div class="collapse p-1" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                        <nav class="sb-sidenav-menu-nested nav drop-link">
-                            <a  href="{{route('scholarship.index')}}" class="linked">Scholarship</a>
-                        </nav>
-                    </div>
-
-                 {{-- @foreach ($menus as $menu)
-
-                    <a class="nav-link" href="{{$menu->nav_link}}">
-                        <div class="sb-nav-link-icon"><i class="{{$menu->icon}} text-white fa-lg"></i></div>
-                        {{$menu->nav_name}}
-                    </a>
-                    @endforeach --}}
-                    <a class="nav-link" href="{{route('contact.index')}}">
-                        <div class="sb-nav-link-icon"><i class="fa-regular fa-message fa-fw text-white fa-lg"></i></div>
-                        Messages
-                    </a>
-
-                    <a class="nav-link" href="{{route('faculty.index')}}">
-                        <div class="sb-nav-link-icon"><i class="fa-solid fa-user-graduate text-white fa-lg"></i></div>
-                        Faculty
-                    </a>
-                    <a class="nav-link" href="{{route('Courses.index')}}">
-                        <div class="sb-nav-link-icon"><i class="fa-regular fa-newspaper text-white fa-lg"></i></div>
-                        Course
-                    </a>
-
-                    <a class="nav-link" href="{{url('/dashboard/routine')}}">
-                        <div class="sb-nav-link-icon"><i class="fa-regular fa-calendar-check text-white fa-lg" ></i></div>
-                        Routine
-                    </a>
-                    <a class="nav-link" href="{{route('schedules.index')}}">
-                        <div class="sb-nav-link-icon"><i class="fa-solid fa-clipboard-list text-white fa-lg"></i></div>
-                        Schedule
-                    </a>
-                    <a class="nav-link" href="{{route('event.index')}}">
-                        <div class="sb-nav-link-icon"><i class="fa-solid fa-calendar-days text-white fa-lg"></i></div>
-                        Events
-                    </a>
-
-
-                    <a class="nav-link" href="">
-                        <div class="sb-nav-link-icon"><i class="fa-solid fa-award  text-white fa-lg"></i></div>
-                        Communication
-                    </a>
-
-                    <a class="nav-link" href="{{route('staff.index')}}">
-                        <div class="sb-nav-link-icon"><i class="fa-regular fa-address-book text-white fa-lg"></i></div>
-                        Staff
-                    </a>
-                    <a class="nav-link" href="{{route('notice.index')}}">
-                        <div class="sb-nav-link-icon"><i class="fa-solid fa-circle-info text-white fa-lg"></i></div>
-                        Notice Board
-                    </a>
-
-                    {{-- <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
-                        <div class="sb-nav-link-icon"><i class="fa-solid fa-gear text-white fa-lg"></i></div>
-                        Setting
-                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down fa-lg"></i></div>
-                    </a>
-                    <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
-                        <nav class="sb-sidenav-menu-nested nav drop-link" id="sidenavAccordionPages">
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
-                                Authentication
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
-                                <nav class="sb-sidenav-menu-nested nav drop-link">
-                                    <a class="nav-link" href="{{route('login')}}">Login</a>
-                                    <a class="nav-link" href="{{route('register')}}">Register</a>
-                                    <a class="nav-link" href="{{route('password.request')}}">Forgot Password</a>
-                                </nav>
-                            </div>
-
-                        </nav>
-                    </div>
-                    <a class="nav-link" href="{{route('index')}}">
-                        <div class="sb-nav-link-icon"><i class=" text-white fa-lg"></i></div>
-                        Menu Permission
-                    </a>
-                    <div class="sb-sidenav-menu-heading">Additional</div>
-
-                    <a class="nav-link" href="{{route('chart')}}">
-                        <div class="sb-nav-link-icon"><i class="fa-solid fa-circle-info text-white fa-lg"></i></div>
-                        Notice Board
-                    </a>
-                    {{-- <a class="nav-link" href="{{route('chart')}}">
-                        <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
-                        Charts
-                    </a>
-                    <a class="nav-link" href="{{route('static')}}">
-                        <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                        Tables
-                    </a> --}}
-                </div>
-            </div>
-        </nav>
-    </div>
-
-
-
-
-
-
-{{--
-
- <div id="layoutSidenav_nav">
     <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
         <div class="sb-sidenav-menu">
             <div class="nav">
-                <div class="sb-sidenav-menu-heading">Core</div>
-                <a class="nav-link" href="index.html">
-                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+
+                <!-- Dashboard Link -->
+                <a class="nav-link" href="{{route('dashboard')}}">
+                    <div class="sb-nav-link-icon text-white"><i class="fas fa-tachometer-alt fa-lg"></i></div>
+                    Dashboard
+                </a>
+
+                <!-- Users Dropdown -->
+                <a class="nav-link collapsed mt-3" href="#" data-bs-toggle="collapse" data-bs-target="#collapseUsers" aria-expanded="false" aria-controls="collapseUsers">
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-users text-white fa-lg"></i></div>
+                    Users
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="collapseUsers" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a href="{{route('users.index')}}" class="nav-link">Admins</a>
+                        <a href="#" class="nav-link">Upcoming</a>
+                    </nav>
+                </div>
+
+                <!-- Students Dropdown -->
+                <a class="nav-link collapsed mt-1" href="#" data-bs-toggle="collapse" data-bs-target="#collapseStudents" aria-expanded="false" aria-controls="collapseStudents">
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-book-open fa-lg text-white"></i></div>
+                    Students
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="collapseStudents" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a href="{{route('student.index')}}" class="nav-link">Students</a>
+                        <a href="{{route('alumni.index')}}" class="nav-link">Alumni</a>
+                        <a href="{{route('scholarship.index')}}" class="nav-link">Scholarship</a>
+                    </nav>
+                </div>
+
+                <!-- Website Setup Dropdown -->
+                <a class="nav-link collapsed mt-1 text-white fa-lg" href="#" data-bs-toggle="collapse" data-bs-target="#collapseWebsiteSetup" aria-expanded="false" aria-controls="collapseWebsiteSetup">
+                    <div class="sb-nav-link-icon  text-white fa-lg"><svg id="Group_28315" data-name="Group 28315" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+                        <circle id="Ellipse_893" data-name="Ellipse 893" cx="0.625" cy="0.625" r="0.625" transform="translate(7.375 6.125)" fill="#575b6a"></circle>
+                        <path id="Path_40777" data-name="Path 40777" d="M13.5,0H2.5A2.5,2.5,0,0,0,0,2.5V11a2.5,2.5,0,0,0,2.5,2.5H7.375v1.25H5.5A.625.625,0,0,0,5.5,16h5a.625.625,0,0,0,0-1.25H8.625V12.875A.625.625,0,0,0,8,12.25H2.5A1.251,1.251,0,0,1,1.25,11V2.5A1.251,1.251,0,0,1,2.5,1.25h11A1.251,1.251,0,0,1,14.75,2.5V11a1.251,1.251,0,0,1-1.25,1.25h-3a.625.625,0,0,0,0,1.25h3A2.5,2.5,0,0,0,16,11V2.5A2.5,2.5,0,0,0,13.5,0Z" fill="#575b6a"></path>
+                        <path id="Path_40778" data-name="Path 40778" d="M120.375,84.75a.625.625,0,0,0,.625-.625v-.688a3.107,3.107,0,0,0,1.1-.456l.487.487a.625.625,0,0,0,.884-.884l-.487-.487a3.108,3.108,0,0,0,.456-1.1h.688a.625.625,0,1,0,0-1.25h-.688a3.108,3.108,0,0,0-.456-1.1l.487-.487a.625.625,0,0,0-.884-.884l-.487.487a3.107,3.107,0,0,0-1.1-.456v-.688a.625.625,0,0,0-1.25,0v.688a3.108,3.108,0,0,0-1.1.456l-.487-.487a.625.625,0,0,0-.884.884l.487.487a3.108,3.108,0,0,0-.456,1.1h-.688a.625.625,0,0,0,0,1.25h.688a3.108,3.108,0,0,0,.456,1.1l-.487.487a.625.625,0,0,0,.884.884l.487-.487a3.107,3.107,0,0,0,1.1.456v.688A.625.625,0,0,0,120.375,84.75ZM118.5,80.375a1.875,1.875,0,1,1,1.875,1.875A1.877,1.877,0,0,1,118.5,80.375Z" transform="translate(-112.375 -73.625)" fill="#575b6a"></path>
+                    </svg></div>
+                    Website Setup
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="collapseWebsiteSetup" aria-labelledby="headingThree" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a href="#" class="nav-link">Hero Section</a>
+                        <a href="#" class="nav-link">Feature</a>
+                        <a href="#" class="nav-link">Services</a>
+                        <a href="#" class="nav-link">Footer</a>
+                        <a href="#" class="nav-link">Appearance</a>
+                    </nav>
+                </div>
+
+                <!-- Messages Link -->
+                <a class="nav-link" href="{{route('contact.index')}}">
+                    <div class="sb-nav-link-icon"><i class="fa-regular fa-message fa-fw text-white fa-lg"></i></div>
+                    Messages
+                </a>
+
+                <!-- Faculty Link -->
+                <a class="nav-link" href="{{route('faculty.index')}}">
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-user-graduate text-white fa-lg"></i></div>
+                    Faculty
+                </a>
+
+                <!-- Courses Link -->
+                <a class="nav-link" href="{{route('Courses.index')}}">
+                    <div class="sb-nav-link-icon"><i class="fa-regular fa-newspaper text-white fa-lg"></i></div>
+                    Course
+                </a>
+
+                <!-- Routine Link -->
+                <a class="nav-link" href="{{url('/dashboard/routine')}}">
+                    <div class="sb-nav-link-icon"><i class="fa-regular fa-calendar-check text-white fa-lg"></i></div>
+                    Routine
+                </a>
+
+                <!-- Schedule Link -->
+                <a class="nav-link" href="{{route('schedules.index')}}">
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-clipboard-list text-white fa-lg"></i></div>
+                    Schedule
+                </a>
+
+                <!-- Events Link -->
+                <a class="nav-link" href="{{route('event.index')}}">
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-calendar-days text-white fa-lg"></i></div>
+                    Events
+                </a>
+
+                <!-- Communication Link -->
+                <a class="nav-link" href="#">
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-award text-white fa-lg"></i></div>
+                    Communication
+                </a>
+
+                <!-- Staff Link -->
+                <a class="nav-link" href="{{route('staff.index')}}">
+                    <div class="sb-nav-link-icon"><i class="fa-regular fa-address-book text-white fa-lg"></i></div>
+                    Staff
+                </a>
+
+                <!-- Notice Board Link -->
+                <a class="nav-link" href="{{route('notice.index')}}">
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-circle-info text-white fa-lg"></i></div>
+                    Notice Board
+                </a>
+            </div>
+        </div>
+    </nav>
+</div>
+
+                        {{-- <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+                            <div class="sb-nav-link-icon"><i class="fa-solid fa-gear text-white fa-lg"></i></div>
+                            Setting
+                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down fa-lg"></i></div>
+                        </a>
+                        <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+                            <nav class="sb-sidenav-menu-nested nav drop-link" id="sidenavAccordionPages">
+                                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
+                                    Authentication
+                                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                                </a>
+                                <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
+                                    <nav class="sb-sidenav-menu-nested nav drop-link">
+                                        <a class="nav-link" href="{{route('login')}}">Login</a>
+                                        <a class="nav-link" href="{{route('register')}}">Register</a>
+                                        <a class="nav-link" href="{{route('password.request')}}">Forgot Password</a>
+                                    </nav>
+                                </div>
+
+                            </nav>
+                        </div>
+                        <a class="nav-link" href="{{route('index')}}">
+                            <div class="sb-nav-link-icon"><i class=" text-white fa-lg"></i></div>
+                            Menu Permission
+                        </a>
+                        <div class="sb-sidenav-menu-heading">Additional</div>
+
+                        <a class="nav-link" href="{{route('chart')}}">
+                            <div class="sb-nav-link-icon"><i class="fa-solid fa-circle-info text-white fa-lg"></i></div>
+                            Notice Board
+                        </a>
+                        {{-- <a class="nav-link" href="{{route('chart')}}">
+                            <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                            Charts
+                        </a>
+                        <a class="nav-link" href="{{route('static')}}">
+                            <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                            Tables
+                        </a> --}}
+
+
+
+
+
+
+    {{--
+
+    <div id="layoutSidenav_nav">
+        <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+            <div class="sb-sidenav-menu">
+                <div class="nav">
+                    <div class="sb-sidenav-menu-heading">Core</div>
+                    <a class="nav-link" href="index.html">
+                        <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                     Dashboard
                 </a>
                 <div class="sb-sidenav-menu-heading">Interface</div>
